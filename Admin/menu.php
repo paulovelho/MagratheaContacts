7f93ff643d20ac2a04c4d7acdb396f460e1c8257
paulovelho <paulovelho@paulovelho.com>
2015-02-16 17:28:42 +0000

    addmail created

--- Admin/menu.php
+++ Admin/menu.php
@@ -1,4 +1,5 @@
 <ul>
 	<li><a href="showMail.php"><i class="fa fa-envelope-square"></i> Show Mails</a></li>
 	<li><a href="showSources.php"><i class="fa fa-sun-o"></i> Show Sources</a></li>
+	<li><a href="addMail.php"><i class="fa fa-paper-plane-o"></i> Add Mail</a></li>
 </ul>
