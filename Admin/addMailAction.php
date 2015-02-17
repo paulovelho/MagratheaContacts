9a6362c49941bf6efd9e15815eee4395a9817e73
paulovelho <paulovelho@paulovelho.com>
2015-02-17 18:14:09 +0000

    addMailAction

---
+++ Admin/addMailAction.php
@@ -0,0 +1,34 @@
+<?php
+
+	$data = $_POST;
+	$email = new Email();
+	$email->source_id = $data["source_id"];
+	$email->email_from = $data["email_from"];
+	$email->email_replyTo = $data["email_replyTo"];
+	$email->email_to = $data["email_to"];
+	$email->msg_subject = $data["msg_subject"];
+	$email->message = $data["message"];
+	$email->priority = $data["priority"];
+	$email->Insert();
+
+	if(empty($email->id)){
+		?>
+		<div class="alert alert-error">
+			<button class="close" data-dismiss="alert" type="button">×</button>
+			<strong>Error adding mail</strong></br/>
+			It seems that something wrong happened!
+		</div>
+		<?
+	} else {
+		?>
+		<div class="alert alert-success">
+			<button class="close" data-dismiss="alert" type="button">×</button>
+			<strong>Success!</strong><br/>
+			E-mail added to table: <br/>
+			<textarea class="textarea_large"><?=p_r($email)?></textarea><br/>
+		</div>
+		<?
+	}
+
+
+?>
