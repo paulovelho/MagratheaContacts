471006029bf444d545dab8a7b713eb6579825986
paulovelho <paulovelho@paulovelho.com>
2015-02-20 18:12:24 +0000

    webservice call on index

--- app/index.php
+++ app/index.php
@@ -3,12 +3,45 @@
 	include("inc/global.php");
 
 	include("Controls/_Controller.php");
-
-
 	// plugins:
 	include("plugins/bootstrap/load.php");
 
+	$wsContato = MagratheaConfig::Instance()->GetFromDefault("server_url");
+
 //	p_r($_GET);
+
+	$data = @$_POST;
+	if(!empty($data)){
+		$name = $data["name"];
+		$email = $data["email"];
+		$subject = $data["subject"];
+		$message = $data["message"];
+
+		$email_message = "FROM: [".$name." <".$email.">] :  \n\n".$message;
+		$postData = array(
+			'source' => 2,
+			'to' => 'magrathea_contacts@paulovelho.com',
+			'replyto' => "'".$name."' <".$email.">",
+			'subject' => $subject,
+			'message' => $email_message,
+			'priority' => 70 );
+		$options = array(
+			'http' => array(
+			'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+			'method'  => 'POST',
+			'content' => http_build_query($postData),
+			),
+		);
+		$context  = stream_context_create($options);
+		$wsContato = $wsContato."/server.php?addMail";
+		$result = file_get_contents($wsContato, false, $context);
+
+		$successMessage = "<div class=\"alert alert-success\">" .
+				"<button class=\"close\" data-dismiss=\"alert\" type=\"button\">×</button>" .
+				"Message sent!</div>";
+		$Smarty->assign("message", $successMessage);
+	}
+
 
 	BaseControl::Form();
 
