4a27e9d60408fbb93f7d251c4223011feb22ef12
Paulo Henrique Martins <paulovelho@paulovelho.com>
2015-05-14 15:40:56 +0100

    Update Admin.php
    adding md5 to password

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,7 +8,7 @@ class Admin extends AdminBase {
 
 class AdminControl extends AdminControlBase {
 	public static function getByEmailAndPassword($e, $p){
-		$q = MagratheaQuery::Select()->Table("admin")->Where(array("email"=>$e, "password"=>$p));
+		$q = MagratheaQuery::Select()->Table("admin")->Where(array("email"=>$e, "password"=>md5($p)));
 		return self::Run($q);
 	}
 }
