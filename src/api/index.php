0fee6c6de5ad83cacc9328a71a96380b78781785
Paulo <paulovelho@paulovelho.com>
2022-12-26 14:55:30 -0300

    docker update and models v1

--- src/api/index.php
+++ src/api/index.php
@@ -6,6 +6,6 @@ $appRoot = realpath(dirname(__FILE__));
 Magrathea2\MagratheaPHP::Instance()->AppPath($appRoot)->Load();
 
 $bootstrap = Magrathea2\Bootstrap\Start::Instance();
-$bootstrap->setPath($appRoot)->Load();
+$bootstrap->Load();
 
 ?>
