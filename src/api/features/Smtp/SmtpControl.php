8e78747cd643836c8c50ec145830c443f47e8c37
Paulo Martins <paulovelho@paulovelho.com>
2023-12-15 00:16:19 -0300

    testing none on smtp

--- src/api/features/Smtp/SmtpControl.php
+++ src/api/features/Smtp/SmtpControl.php
@@ -5,7 +5,7 @@ class SmtpControl extends \MagratheaContacts\Smtp\Base\SmtpControlBase {
 
 	public static function GetSelectWithNone() {
 		$arr = parent::GetSelectArray();
-		$arr[0] = "none";
+		$arr[0] = "";
 		return $arr;
 	}
 
