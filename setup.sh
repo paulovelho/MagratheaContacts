fc8552d2471df481f316c671e36357758135b620
Paulo Martins <paulovelho@paulovelho.com>
2026-07-07 19:21:38 -0300

    setup.sh fixed

--- setup.sh
+++ setup.sh
@@ -20,26 +20,10 @@ echo "Created app-admin/public/config.json"
 sed -i "s|default: .*|default: $server_url|" app-admin/public/openapi.yaml
 echo "Updated apiUrl default in app-admin/public/openapi.yaml"
 
-# Copy the files into src/api/admin
-dir_build="app-admin/public/"
+# Copy the just-updated config.json and openapi.yaml into src/api/admin
 dir_admin="src/api/admin/"
 
-config_backup=""
-if [ -f "${dir_admin}config.json" ]; then
-	config_backup=$(cat "${dir_admin}config.json")
-fi
-
-rm -rf "$dir_admin"
-cp -rv "$dir_build" "$dir_admin"
-
-if [ -n "$config_backup" ]; then
-	echo "$config_backup" > "${dir_admin}config.json"
-else
-	cat > "${dir_admin}config.json" <<EOF
-{
-	"apiUrl": "$server_url"
-}
-EOF
-fi
+cp -v app-admin/public/config.json "${dir_admin}config.json"
+cp -v app-admin/public/openapi.yaml "${dir_admin}openapi.yaml"
 
 echo "Setup complete. ${dir_admin}config.json set to apiUrl: $server_url"
